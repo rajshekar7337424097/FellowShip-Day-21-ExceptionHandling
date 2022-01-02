bea9c9bbeb6da30e9ad542c8fe74f71319f198a7
rajashekar reddy <chrajashekarreddy777@gmail.com>
2022-01-02 17:36:34 +0530

    password with rule 1 ia passed

---
+++ src/com/bl/userregistration/PasswordRule1.java
@@ -0,0 +1,30 @@
+package com.bl.userregistration;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PasswordRule1 {
+	
+	public static boolean passwordRule1(String inputStr) {
+		String passwordPattern = "^[a-z]{8}$";
+		Pattern regex = Pattern.compile(passwordPattern);
+		Matcher passwordMatcher = regex.matcher(inputStr);
+		if(passwordMatcher.matches()) {
+			System.out.println(inputStr+"Rule 1 is passed and Succesfully loggedin");
+			return true;
+		}else {
+			System.out.println(inputStr+"is a Invalid input");
+			return false;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Password must be 8 character password.");
+		System.out.println("Enter Password :");
+		String inputStr = sc.next();
+		sc.close();
+		passwordRule1(inputStr);
+	}
+}
