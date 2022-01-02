97f8e6e62d1073107877760bd8b1ae0070b52efa
rajashekar reddy <chrajashekarreddy777@gmail.com>
2022-01-02 17:40:14 +0530

    password rule3 is passed and adding intto remote repo

---
+++ src/com/bl/userregistration/PasswordRule3.java
@@ -0,0 +1,32 @@
+package com.bl.userregistration;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PasswordRule3 {
+	
+	public static boolean passwordRule3(String inputStr) {
+		String passwordPattern = "^[A-Za-z]{8}[0-9]{1}$";
+		Pattern regex = Pattern.compile(passwordPattern);
+		Matcher passwordMatcher3 = regex.matcher(inputStr);
+		
+		if(passwordMatcher3.matches()) {
+			System.out.println(inputStr+" Rule 2 is passed and succesfully loggedin.");
+			return true;
+		}else {
+			System.out.println(inputStr+" is a Invalid input ");
+			return false;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Password must be 8 characher and having atleast one numerics");
+		System.out.println("Enter Password :");
+		String inputStr = sc.next();
+		sc.close();
+		
+		passwordRule3(inputStr);	
+	}
+}
